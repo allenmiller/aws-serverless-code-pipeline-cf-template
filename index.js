202927d694bdb5d8f4df197f27b801e69de06d2d
Al Miller <al.miller@ajmiller.net>
2019-07-10 22:38:35 -0700

    update lambda

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello World from the Hood Canal!')
 })
 
 module.exports.handler = serverless(app);
